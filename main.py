c6cf671553f423a981358ddb526e5b58db6397a5
Lakshitha Deshan <notlakshitha@gmail.com>
2024-12-18 23:34:04 +0530

    test

--- main.py
+++ main.py
@@ -17,20 +17,21 @@ app=Flask(__name__)
 generation_config = {
   "temperature": 1,
   "top_p": 0.95,
-  "top_k": 0,
+  "top_k": 40,
   "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
 }
 
-safety_settings = [
-  {"category": "HARM_CATEGORY_HARASSMENT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-  {"category": "HARM_CATEGORY_HATE_SPEECH","threshold": "BLOCK_MEDIUM_AND_ABOVE"},  
-  {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-  {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-]
+# safety_settings = [
+#   {"category": "HARM_CATEGORY_HARASSMENT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+#   {"category": "HARM_CATEGORY_HATE_SPEECH","threshold": "BLOCK_MEDIUM_AND_ABOVE"},  
+#   {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+#   {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+# ]
 
 model = genai.GenerativeModel(model_name=model_name,
-                              generation_config=generation_config,
-                              safety_settings=safety_settings)
+                              generation_config=generation_config
+                              )
 
 convo = model.start_chat(history=[
 ])
