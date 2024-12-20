2ed30d69c8e8e0d6b0e778c265140e0b3459a1f5
Lakshitha Deshan <notlakshitha@gmail.com>
2024-12-20 21:33:58 +0530

    test

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from flask import Flask, request, jsonify
 import requests
 import os
 import fitz
-from dotenv import load_dotenv
 
-load_dotenv()
 
 wa_token = os.environ.get("WA_TOKEN")
 genai.configure(api_key=os.environ.get("GEN_API"))
@@ -24,17 +22,12 @@ generation_config = {
     "max_output_tokens": 8192,
 }
 
-safety_settings = [
-    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-]
+
 
 model = genai.GenerativeModel(
     model_name=model_name,
     generation_config=generation_config,
-    safety_settings=safety_settings,
+   
 )
 
 convo = model.start_chat(history=[])
